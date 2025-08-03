f0ec2d981d5d2dda9ef5f59398690a1fdcf93a18
sonitambashta <sunnyambashta@gmail.com>
2025-08-03 00:56:11 -0400

    Added background with green translucent overlay

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 const Navbar = () => {
     /* If user is not logged in*/
     return (
-        <nav>
+        <nav className="navbar bg-green-500 p-4">
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/login">Login</a></li>
-                <li><a href="/register">Register</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/board">Board</a></li>
+                <li className="nav-item"><a href="/">Home</a></li>
+                <li className="nav-item"><a href="/login">Login</a></li>
+                <li className="nav-item"><a href="/register">Register</a></li>
+                <li className="nav-item"><a href="/about">About</a></li>
+                <li className="nav-item"><a href="/board">Board</a></li>
             </ul>
         </nav>
     )
